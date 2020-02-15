e4f4326e9b7e6bf5727d10be92c00e629c17dc19
Jonas Reif <jonas.reif@stud.uni-regensburg.de>
2020-02-15 10:45:48 +0100

    Optimize images for audit

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,6 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato&display=swap' }
-    ]
   },
   /*
   ** Customize the progress-bar color
@@ -48,7 +45,14 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    'nuxt-webfontloader',
   ],
+
+  webfontloader: {
+    google: {
+      families: ['Lato:400,700&display=swap'] //Loads Lato font with weights 400 and 700
+    }
+  },
   /*
   ** Build configuration
   */
