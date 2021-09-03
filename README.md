23061cf560edc35aad2d1d7de2e5b6be882723f6
Jonas Reif <jonas@upspeak.de>
2021-09-03 12:30:33 +0200

    Update readme and about text

--- README.md
+++ README.md
@@ -11,12 +11,17 @@ $ npm run install
 # serve with hot reload at localhost:3000
 $ npm run dev
 
-# build for production and launch server
-$ npm run build
-$ npm run start
-
 # generate static project
 $ npm run generate
+
+# install firebase cli
+$ npm install -g firebase-tools
+
+# login to firebase (jonas account)
+$ firebase login
+
+# deploy hosting
+$ firebase deploy
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- pages/ueber-mich.vue
+++ pages/ueber-mich.vue
@@ -5,12 +5,17 @@
     <img class="-mt-12 z-0 inline-block" src="~/assets/img/underline.png"/>
 
     <!--Content-->
-    <div class="mt-4 flex flex-wrap -mx-2 overflow-hidden text-gray-700 font-body">
+    <div class="mt-4 flex flex-wrap -mx-2 overflow-hidden text-gray-700 font-body text-left">
       <div class="my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
         <p class="font-body">
           Mein Name ist Lisa, ich bin 30 Jahre alt und Mama von zwei Kindern. 
+          <br />
+          <br />
           Durch meine Tochter bin ich 2018 zum Tragen gekommen und habe mich im September 2019 entschlossen, 
-          eine Ausbildung zur Trageberaterin bei der Trageschule Hamburg zu machen. Seit Januar 2020 bin ich zertifizierte Trageberaterin.
+          eine Ausbildung zur Trageberaterin bei der Trageschule Hamburg zu machen. 
+          <br />
+          <br /> 
+          Seit Januar 2020 bin ich zertifizierte Trageberaterin.
         </p>
       </div>
       <div class="my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
